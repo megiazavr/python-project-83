9171b6c53fd801d15e3df95f9fdf148d817953db
megiazavr <milena.egiazaryan77@mail.ru>
2024-12-28 21:22:58 +0000

    flake8 correction

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,16 +1,16 @@
 import os
-from flask import Flask, render_template
+from flask import Flask
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 
-'default_secret_key')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key')
+
 
 def index():
     return "Welcome to Page Analyzer!"
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-
